044637acbaa12cecce59bbe6f847277b87eb2dc6
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-09 00:31:13 +0600

    fixed several issue

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -2,9 +2,6 @@ import Image from "next/image";
 import React from "react";
 import SamiulAlim from "../../images/samiul-alim.png";
 
-const base64 =
-    "iVBORw0KGgoAAAANSUhEUgAABNgAAATQCAMAAAAWOwgjAAADAFBMVEUAAAAKEzENGDwLFTUQIU8PHUYRHD4PHkkFCyYTJFQZO4MLGEANHUoULGwKEzAUKGEQIE4SJVkPIVMPGjoMGD8MG";
-
 const Hero = () => {
     return (
         <div
@@ -30,10 +27,7 @@ const Hero = () => {
                         I build things for the web.
                     </h3>
                     <p className="py-6 text-base text-center md:text-xl sm:text-left sm:w-10/12">
-                        I&apos;m a software engineer specializing in building
-                        (and occasionally designing) exceptional digital
-                        experiences. Currently, I&apos;m focused on building
-                        accessible, human-centered products at Upstatement.
+                        I dream to be an expert software engineer. In-depth knowledge of Node JS, React, NextJS, and MongoDB. 2 years of experience efficiently coding websites and applications using modern JavaScript, Typescript, back-end, and front-end developer. Building state-of-the-art, easy-to-use, user-friendly websites and applications is truly a passion, and I am confident I would be an excellent addition to your organization.
                     </p>
 
                     <div className="flex justify-center sm:justify-start">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <meta property="og:title" content="Samiul Alim" />
                 <meta
                     property="og:description"
-                    content="I dream of being an expert software engineer or a web developer. Excellent reputation for resolving problems and improving customer satisfaction. In-depth knowledge of Node JS, React, NextJS and MongoDB. Skillful in creating servers and databases for functionality and designing and developing APIs. 2 years of experience efficiently coding websites and applications using modern HTML, CSS, JavaScript, Typescript and React and as a back-end and a front-end developer. Building state-of-the-art, easy-to-use, user-friendly websites and applications is truly a passion of mine and I am confident I would be an excellent addition to your organization. In addition to my knowledge base, I actively seek out new technologies and stay up-to-date on industry trends and advancements."
+                    content="I dream to be an expert software engineer. In-depth knowledge of Node JS, React, NextJS, and MongoDB. 2 years of experience efficiently coding websites and applications using modern JavaScript, Typescript, back-end, and front-end developer. Building state-of-the-art, easy-to-use, user-friendly websites and applications is truly a passion, and I am confident I would be an excellent addition to your organization."
                 />
                 <meta property="og:image" content="/Samiul-Alim.png" />
             </Head>
