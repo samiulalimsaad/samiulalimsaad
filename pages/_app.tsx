5ddc8e916f109e3ad590dc4c3325cd54069a06d4
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-09 00:19:09 +0600

    web preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title>Samiul Alim</title>
-                <meta name="description" content="Samiul Alim Saad Portfolio" />
                 <link rel="icon" href="/samiul-alim.ico" />
 
                 <meta
@@ -19,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <meta property="og:title" content="Samiul Alim" />
                 <meta
                     property="og:description"
-                    content="Samiul Alim | Portfolio | Web Developer | facebook.com/samiulalimsaad | m.me/samiulalimsaad | linkedin.com/in/samiulalimsaad | github.com/samiulalimsaad | portfolio-saadraj.vercel.app"
+                    content="I dream of being an expert software engineer or a web developer. Excellent reputation for resolving problems and improving customer satisfaction. In-depth knowledge of Node JS, React, NextJS and MongoDB. Skillful in creating servers and databases for functionality and designing and developing APIs. 2 years of experience efficiently coding websites and applications using modern HTML, CSS, JavaScript, Typescript and React and as a back-end and a front-end developer. Building state-of-the-art, easy-to-use, user-friendly websites and applications is truly a passion of mine and I am confident I would be an excellent addition to your organization. In addition to my knowledge base, I actively seek out new technologies and stay up-to-date on industry trends and advancements."
                 />
                 <meta property="og:image" content="/Samiul-Alim.png" />
             </Head>
