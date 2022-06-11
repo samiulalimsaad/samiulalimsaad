2880d351e52880bf4053cdd6b514c04588916d1c
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-11 12:56:14 +0600

    temporary added project section

--- components/Projects/Temp.tsx
+++ components/Projects/Temp.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import { BiLinkExternal } from "react-icons/bi";
+import { BsGithub } from "react-icons/bs";
+import { FaServer } from "react-icons/fa";
 import { projectInterface } from "./projects";
 
-const Temp = ({ project }: { project: projectInterface }) => {
+const Temp = ({
+    project,
+    index,
+}: {
+    project: projectInterface;
+    index: number;
+}) => {
     return (
         <div
             className="shadow-xl card lg:card-sides bg-base-100 glass"
@@ -12,9 +21,11 @@ const Temp = ({ project }: { project: projectInterface }) => {
                 <Image src={project.image} alt="Album" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">{project.name}</h2>
+                <h2 className="card-title">
+                    {project.name} - {index}
+                </h2>
                 <div className="text-justify">{project.shortDescription}</div>
-                <div className="flex justify-between">
+                <div className="flex flex-wrap justify-between cursor-vertical-text">
                     {project.tools.map((v) => (
                         <p
                             key={v}
@@ -24,7 +35,36 @@ const Temp = ({ project }: { project: projectInterface }) => {
                         </p>
                     ))}
                 </div>
-                <div className="justify-end card-actions">
+                <div className="items-center justify-between card-actions">
+                    <div className="flex items-center justify-between gap-2">
+                        <a
+                            href="https://www.github.com/samiulalimsaad"
+                            target="_blank"
+                            rel="noreferrer"
+                            data-aos="flip-left"
+                            data-aos-duration="3000"
+                        >
+                            <BsGithub className="w-6 h-6 text-green-500 duration-500 hover:text-green-600" />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/samiulalimsaad"
+                            target="_blank"
+                            rel="noreferrer"
+                            data-aos="flip-right"
+                            data-aos-duration="3000"
+                        >
+                            <FaServer className="w-6 h-6 duration-500 text-sky-500 hover:text-sky-600" />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/samiulalimsaad"
+                            target="_blank"
+                            rel="noreferrer"
+                            data-aos="flip-right"
+                            data-aos-duration="3000"
+                        >
+                            <BiLinkExternal className="w-6 h-6 text-blue-500 duration-500 hover:text-blue-600" />
+                        </a>
+                    </div>
                     <button className="btn btn-primary">Details</button>
                 </div>
             </div>
--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -5,17 +5,33 @@ const Projects = () => {
     return (
         <section
             id="projects"
-            className="min-h-fit hero bg-[url('/project.png')] "
+            className="min-h-fit hero bg-[url('/project.png')] overflow-hidden"
         >
-            <div className="w-full h-full py-20 mx-auto bg-slate-900/60">
+            <div className="w-full h-full py-20 mx-auto bg-slate-900/60 hover:backdrop-blur-sm">
                 <h2 className="my-8 text-6xl font-black text-center">
                     Projects
                 </h2>
                 <div className="divider"></div>
-                <div className="grid justify-between w-11/12 grid-cols-3 gap-4 mx-auto">
-                    {projects.slice(0, 6).map((v) => (
-                        <Temp key={Date.now()} project={v} />
-                    ))}
+                <div className="w-10/12 min-h-screen mx-auto hero">
+                    {/* <div className="flex-col w-11/12 hero-content lg:flex-row">
+                        <div
+                            className="bg-slate-50 rounded-3xl"
+                            data-aos="zoom-out-down"
+                            data-aos-duration="3000"
+                        >
+                            <Image
+                                src={project}
+                                alt="Exp"
+                                className="w-48 sm:w-96"
+                            />
+                        </div>
+                    </div> */}
+                    <div className="grid gap-4 lg:grid-cols-3">
+                        {/* <div className="w-full carousel"> */}
+                        {projects.slice(0, 6).map((v, i) => (
+                            <Temp key={i} project={v} index={i + 1} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </section>
