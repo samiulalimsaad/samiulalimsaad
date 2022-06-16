fb951ca63e1d5aba850c504443cb2977e237170c
samiulalimsaad <saadnbiuraj@gmail.com>
2022-06-17 00:44:27 +0600

    mongoose added

--- backend/Database.ts
+++ backend/Database.ts
@@ -1,25 +1,15 @@
-// const connectDB = (handler) => async (req, res) => {
-//     if (mongoose.connections[0].readyState) {
-//         // Use current db connection
-//         return handler(req, res);
-//     }
-//     // Use new db connection
-//     await mongoose.connect(process.env.mongodburl, {
-//         useUnifiedTopology: true,
-//         useFindAndModify: false,
-//         useCreateIndex: true,
-//         useNewUrlParser: true,
-//     });
-//     return handler(req, res);
-// };
-
+import mongoose from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextConnect } from "next-connect";
 
 const connectDB =
     (handler: NextConnect<NextApiRequest, NextApiResponse<any>>) =>
-    async (req: NextApiRequest, res: NextApiResponse) => {
-        console.log("connected");
+    (req: NextApiRequest, res: NextApiResponse) => {
+        console.log("database triggered");
+        if (mongoose.connections[0].readyState) {
+            return handler(req, res);
+        }
+        mongoose.connect(process.env.DATABASE_URL as string);
         return handler(req, res);
     };
 
