0bb35a8df5a694d8e1d29a79cacc096ec9d12311
samiulalimsaad <saadnbiuraj@gmail.com>
2022-07-24 12:14:02 +0600

    experience added

--- components/Experience/index.tsx
+++ components/Experience/index.tsx
@@ -44,7 +44,7 @@ const Experience = () => {
                     Experience
                 </h2>
                 <div className="divider"></div>
-                <div className="flex-col justify-between mx-auto hero-content lg:flex-row">
+                <div className="flex-col items-center justify-between mx-auto hero-content lg:flex-row">
                     <div
                         data-aos="zoom-out-down"
                         data-aos-duration="2000"
@@ -52,50 +52,42 @@ const Experience = () => {
                     >
                         <Image src={Exp} alt="Exp" className="w-48 sm:w-96" />
                     </div>
-                    <div className="p-10 ">
-                        <div>
-                            <ul className="steps steps-vertical">
-                                {experiences.map((experience, i) => (
-                                    <li
-                                        key={experience.dateFrom}
-                                        className="step step-primary"
-                                        data-aos="fade-up"
-                                        data-aos-anchor-placement="center-bottom"
-                                        data-aos-duration="3000"
-                                    >
-                                        <div className="my-8 text-left">
-                                            <h3 className="text-3xl">
-                                                {experience.designation}
-                                            </h3>
-                                            <p className="text-xl">
-                                                at{" "}
+                    <div className="">
+                        <ul className="steps steps-vertical">
+                            {experiences.map((experience) => (
+                                <li
+                                    key={experience.dateFrom}
+                                    className="p-2 step step-success"
+                                    data-aos="fade-up"
+                                    data-aos-anchor-placement="center-bottom"
+                                    data-aos-duration="3000"
+                                >
+                                    <div className="my-8 text-left">
+                                        <h3 className="text-3xl">
+                                            {experience.designation}
+                                        </h3>
+                                        <p className="text-xl">
+                                            at <span>{experience.company}</span>
+                                            , <span>{experience.location}</span>{" "}
+                                            {experience.jobType && (
                                                 <span>
-                                                    {experience.company}
+                                                    ({experience.jobType})
                                                 </span>
-                                                ,{" "}
-                                                <span>
-                                                    {experience.location}
-                                                </span>{" "}
-                                                {experience.jobType && (
-                                                    <span>
-                                                        ({experience.jobType})
-                                                    </span>
-                                                )}
-                                            </p>
-                                            <p className="my-2">
-                                                <span className="p-2 rounded-xl bg-slate-900/60">
-                                                    {experience.dateFrom}
-                                                </span>{" "}
-                                                -{" "}
-                                                <span className="p-2 rounded-xl bg-slate-900/60">
-                                                    {experience.dateTo}
-                                                </span>
-                                            </p>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                                            )}
+                                        </p>
+                                        <p className="my-2">
+                                            <span className="p-2 rounded-xl bg-slate-900/60">
+                                                {experience.dateFrom}
+                                            </span>{" "}
+                                            -{" "}
+                                            <span className="p-2 rounded-xl bg-slate-900/60">
+                                                {experience.dateTo}
+                                            </span>
+                                        </p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
