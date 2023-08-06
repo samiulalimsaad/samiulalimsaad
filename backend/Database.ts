3186ecf35fb4f1a28c619b83207e3b5def999b6f
samiulalimsaad <samiulalimsaad@gmail.com>
2023-08-07 00:05:53 +0600

    db connection issue fixed

--- backend/Database.ts
+++ backend/Database.ts
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextConnect } from "next-connect";
 
-const connectDB =
-    (handler: NextConnect<NextApiRequest, NextApiResponse<any>>) =>
-    (req: NextApiRequest, res: NextApiResponse) => {
-        console.log("database triggered");
-        if (mongoose.connections[0].readyState) {
-            return handler(req, res);
-        }
-        mongoose.connect(process.env.DATABASE_URL as string);
-        return handler(req, res);
-    };
+const connectDB = () => {
+    console.log("database triggered");
+    if (mongoose.connections[0].readyState) {
+        console.log("new connection established for database");
+    }
+    mongoose.connect(process.env.DATABASE_URL as string);
+};
 
 export default connectDB;
