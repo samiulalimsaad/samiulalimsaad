b0db36690cd6f86d207ecec40d585e8310db3fcc
samiulalimsaad <samiulalimsaad@gmail.com>
2023-08-07 01:28:55 +0600

    updated

---
+++ out/_next/static/chunks/app/layout-adcab48019be9852.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5135:function(n,e,t){Promise.resolve().then(t.t.bind(t,8603,23)),Promise.resolve().then(t.t.bind(t,7113,23))},7113:function(){}},function(n){n.O(0,[603,407,437,744],function(){return n(n.s=5135)}),_N_E=n.O()}]);
