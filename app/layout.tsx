e5c6602eca6da95e7c7b5dec80f24fefbacce772
samiulalimsaad <samiulalimsaad@gmail.com>
2023-08-06 10:58:29 +0600

    RootLayout added

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import AOSInit from "../components/AOSInit";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+
+export default function RootLayout({
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <Navbar />
+                <main className="w-full overflow-y-hidden">
+                    <AOSInit>{children}</AOSInit>
+                </main>
+            </body>
+        </html>
+    );
+}
