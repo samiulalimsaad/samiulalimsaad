eb87927e1bb84d33f63788222cce134a7c8c7925
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-04 13:33:25 +0600

    MyImage component added

---
+++ src/components/Skill/MyImage.tsx
@@ -0,0 +1,15 @@
+interface MyImageProps {
+    height: string | number;
+    src: string;
+    alt: string;
+    classNames?: string;
+}
+
+const MyImage = ({ height, src, alt }: MyImageProps) => {
+    return (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img src={src} height={height} className="hover:scale-105" alt={alt} />
+    );
+};
+
+export default MyImage;
