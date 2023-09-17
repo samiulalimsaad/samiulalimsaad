4145e529310b97f7caaaa86512768413b2442a09
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-17 12:32:55 +0600

    fixed

--- src/app/api/projects/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import connectDB from "../../../backend/Database";
-import { ProjectModal } from "../../../backend/Models/Projects.model";
-
-export async function GET(request: Request) {
-    connectDB();
-    const projects = await ProjectModal.find({}).sort({ priority: -1 });
-    return NextResponse.json({ projects });
-}
