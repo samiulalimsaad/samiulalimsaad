dbe7e27674f0754a64e4a436329cd00c57a9dc38
samiulalimsaad <samiulalimsaad@gmail.com>
2023-11-06 18:52:16 +0600

    send to the discord

--- src/backend/visitorCount.ts
+++ src/backend/visitorCount.ts
@@ -1,11 +1,23 @@
+"use server";
+
 const DISCORD_MENTION_ID = process.env.NEXT_APP_DISCORD_MENTION_ID;
 const DISCORD_TOKEN = process.env.NEXT_APP_DISCORD_TOKEN;
 const DISCORD_CHANNEL_ID2 = process.env.NEXT_APP_DISCORD_CHANNEL_ID2;
 
 export async function visitorCount(data: any = {}) {
-    const discordMessage = `<@${DISCORD_MENTION_ID}> a person landed on the site`;
+    const date = new Date();
+    const now = date.toLocaleString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZone: "Asia/Dhaka",
+    });
 
-    await fetch(DISCORD_CHANNEL_ID2!, {
+    const discordMessage = `<@${DISCORD_MENTION_ID}> a person landed on the site at ${now}`;
+
+    fetch(DISCORD_CHANNEL_ID2!, {
         method: "POST",
         body: JSON.stringify({ content: discordMessage }),
         headers: {
