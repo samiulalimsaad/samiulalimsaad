f79f8bbef75c5785eceafad8c8ed4624b27e7bf7
samiulalimsaad <samiulalimsaad@gmail.com>
2023-11-06 18:26:29 +0600

    getSingleProject added

--- src/backend/services/project.service.ts
+++ src/backend/services/project.service.ts
@@ -5,3 +5,8 @@ export const getProjects = async () => {
     await connectDB();
     return await ProjectModal.find({}).sort({ priority: -1 });
 };
+
+export const getSingleProject = async (id: string) => {
+    await connectDB();
+    return await ProjectModal.findById(id);
+};
