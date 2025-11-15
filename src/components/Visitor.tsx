e0d9c07787b4b2947aaef2ce5acd66c05e11e65f
samiulalimsaad <samiulalimsaad@gmail.com>
2025-11-15 19:13:41 +0600

    feat(visitor): Track visits in prod
    - Conditionally track in production
    - Remove console logs

--- src/components/Visitor.tsx
+++ src/components/Visitor.tsx
@@ -2,14 +2,7 @@ import { trackVisitorVisit } from "@/server-actions/visit";
 import { headers } from "next/headers";
 
 export default async function Visitor() {
-    const headersList = headers();
-    const result = await trackVisitorVisit(headersList);
-
-    if (result.success) {
-        console.log("Visitor tracked:", result.ip);
-    } else {
-        console.error("Tracking failed:", result.error);
-    }
+    if (process.env.NODE_ENV === "production") trackVisitorVisit(headers());
 
     return null;
 }
